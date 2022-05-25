9d075718598fd35fb106961c6c71b203280960ec
yanilim1 <103626077+yanilim1@users.noreply.github.com>
2022-05-25 16:27:41 +0800

    Create code.py

---
+++ code.py
@@ -0,0 +1,31 @@
+'''Assignment 0
+
+This assignment is a giveaway assignment.
+By completing this assignment, you will familiarize yourself with this
+    course's submission mechanics.
+'''
+
+def three_number_average(x, y, z):
+  '''Item 1.
+  Three Number Average. 3 points.
+
+  Returns the average of three numbers.
+
+  Parameters
+  ----------
+  x: int
+      the first number
+  y: int
+      the second number
+  z: int
+      the third number
+
+  Returns
+  -------
+  float
+      the average of x, y, and z
+  '''
+  # Write your code below this line
+  return (x + y + z)/3
+  
+print(three_number_average(5, 5, 10)) #6.6666....7
